40734db3b156b52bc702979547aba6ff16da3540
AhmadUbaidWeBDEV <aubaid369369@gmail.com>
2023-05-15 00:28:25 +0500

    php oop

--- admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php include 'header.php'; ?>
-<div class="container-fluid">
-    <h1 style="text-align:center">Welcome Admin</h1>
-</div>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['username']);
-    unset($_SESSION['userrole']);
-    unset($_SESSION['userid']);
-    
-    header('Location: index.php');
-?>
